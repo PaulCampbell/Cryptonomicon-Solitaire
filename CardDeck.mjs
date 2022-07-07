4e014e32f82b6622bc2c085c03a24dd56cf2be39
Paul Campbell <paulcampbell@Pauls-MacBook-Air.local>
2022-07-07 11:44:23 +0100

    refacter cards - they are objects with names and values

--- CardDeck.mjs
+++ CardDeck.mjs
@@ -1,18 +1,12 @@
 class CardDeck {
-  constructor({ key } = {}) {
+  constructor({ cardNamesInOrder } = {}) {
     this.cardNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     this.suits = ["clubs", "diamonds", "hearts", "spades"];
     this.jokers = ["a", "b"];
-    if (key) {
-      this.cards = key;
+    if (cardNamesInOrder) {
+      this.cards = this._constructCards(cardNamesInOrder);
     } else {
-      this.cards = this.suits
-        .reduce((acc, suit) => {
-          return acc.concat(
-            this.cardNumbers.map((number) => `${number} ${suit}`)
-          );
-        }, [])
-        .concat(this.jokers.map((joker) => `${joker} joker`));
+      this.cards = this._freshDeck();
     }
   }
 
@@ -50,6 +44,57 @@ class CardDeck {
     const secondCut = newOrder.slice(firstIndex, lastIndex + 1);
     const lastCut = newOrder.splice(lastIndex + 1, this.cards.length);
     this.cards = firstCut.concat(secondCut).concat(lastCut);
+  }
+
+  _freshDeck() {
+    return this.suits
+      .reduce((acc, suit) => {
+        return acc.concat(
+          this.cardNumbers.map((number) => {
+            return {
+              value: this._getCardValue(suit, number),
+              name: `${number} ${suit}`,
+            };
+          })
+        );
+      }, [])
+      .concat(
+        this.jokers.map((joker) => {
+          return {
+            value: 53,
+            name: `${joker} joker`,
+          };
+        })
+      );
+  }
+
+  _constructCards(cardNamesInOrder) {
+    return cardNamesInOrder.map((name) => {
+      const [number, suit] = name.split(" ");
+      if (suit === "joker") {
+        return {
+          value: 53,
+          name,
+        };
+      } else {
+        return {
+          name,
+          value: this._getCardValue(suit, number),
+        };
+      }
+    });
+  }
+  _getCardValue(suit, number) {
+    switch (suit) {
+      case "clubs":
+        return number;
+      case "diamonds":
+        return number + 13;
+      case "hearts":
+        return number + 26;
+      case "spades":
+        return number + 39;
+    }
   }
 }
 
